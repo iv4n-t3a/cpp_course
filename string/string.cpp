8def667479e3fd7448a5d2d4691b90f6cbaf4064
Иван Чай <138519172+iv4n-t3a@users.noreply.github.com>
2023-10-29 20:24:30 +0000

    Add some functions

--- string/string.cpp
+++ string/string.cpp
@@ -2,12 +2,12 @@
 
 #include <cstdlib>
 #include <cstring>
+#include <utility>
 
 String::String() {
   str_ = static_cast<char*>(calloc(1, sizeof(char)));
   capacity_ = 1;
 }
-
 String::String(size_t size, char character) {
   size_ = size;
   capacity_ = size << 1;
@@ -16,12 +16,27 @@ String::String(size_t size, char character) {
     str_[i] = character;
   }
 }
-
 String::String(char* str) {
   size_ = strlen(str);
   capacity_ = size_ << 1;
   str_ = static_cast<char*>(calloc(capacity_, sizeof(char)));
   memcpy(str_, str, sizeof(char) * size_);
+}
+String::String(String& other) {
+  if (size_ < other.size_) {
+    Resize(other.size_);
+  }
+  memcpy(str_, other.str_, other.size_);
+}
+String::~String() {
+  free(str_);
+}
+String& String::operator=(String& other) {
+  if (size_ < other.size_) {
+    Resize(other.size_);
+  }
+  memcpy(str_, other.str_, other.size_);
+  return *this;
 }
 
 void String::Clear() { size_ = 0; }
@@ -33,14 +48,11 @@ void String::PushBack(char character) {
   str_[size_] = character;
   size_ += 1;
 }
-
 void String::PopBack() { size_ -= 1; }
-
 void String::Resize(size_t size) {
   Reserve(size);
   size_ = size;
 }
-
 void String::Reserve(size_t capacity) {
   capacity_ = capacity;
   char* old_str = str_;
@@ -48,11 +60,37 @@ void String::Reserve(size_t capacity) {
   memcpy(str_, old_str, sizeof(char) * size_);
   free(old_str);
 }
-
 void String::ShrinkToFit() {
   char* old_str = str_;
   str_ = static_cast<char*>(calloc(size_, sizeof(char)));
   memcpy(str_, old_str, sizeof(char) * size_);
   free(old_str);
   capacity_ = size_;
+}
+
+void String::Swap(String& other) {
+  std::swap(str_, other.str_);
+  std::swap(size_, other.size_);
+  std::swap(capacity_, other.capacity_);
+}
+
+
+String String::operator+(String other) {
+  String buf = *this;
+  buf += other;
+  return buf;
+}
+String String::operator+=(String other) {
+  if (other.size_ + size_ > capacity_) {
+    Reserve((other.size_ + size_) << 1);
+  }
+  memcpy(str_ + other.size_, other.str_, other.size_ * sizeof(char));
+  return *this;
+}
+
+String String::operator*(size_t mult) {
+
+}
+String String::operator*=(size_t mult) {
+
 }
--- string/string.hpp
+++ string/string.hpp
@@ -1,23 +1,44 @@
 #include <cstdlib>
 
 class String {
- public:
-  String();
-  String(size_t size, char character);
-  String(char*);
+  public:
+    String();
+    String(size_t size, char character);
+    String(char*);
 
-  void Clear();
+    String(String&);
+    ~String();
+    String& operator=(String&);
 
-  void PushBack(char);
-  void PopBack();
-  void Resize(size_t);
-  void Reserve(size_t);
-  void ShrinkToFit();
 
-  char* Data() { return str_; }
+    void Clear();
+    void PushBack(char);
+    void PopBack();
+    void Resize(size_t);
+    void Reserve(size_t);
+    void ShrinkToFit();
+    void Swap(String&);
 
- private:
-  char* str_ = nullptr;
-  size_t size_ = 0;
-  size_t capacity_ = 0;
+    char& operator[](size_t idx) { return str_[idx]; }
+    char operator[](size_t idx) const { return str_[idx]; }
+    char& Front() { return str_[0]; }
+    char& Back() { return str_[size_ - 1]; }
+    char Front() const { return str_[0]; }
+    char Back() const { return str_[size_ - 1]; }
+
+    bool Empty() const { return size_ == 0; }
+    size_t Size() const { return size_; }
+    size_t Capacity() const { return capacity_; }
+    char* Data() const { return str_; }
+
+    String operator+(String);
+    String operator+=(String);
+
+    String operator*(size_t);
+    String operator*=(size_t);
+
+  private:
+    char* str_ = nullptr;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
 };
