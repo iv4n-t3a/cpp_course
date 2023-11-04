38907e07ca4e76a68b6b9262818a168d1fcf2324
Иван Чай <138519172+iv4n-t3a@users.noreply.github.com>
2023-11-04 22:20:53 +0000

    Write string

--- string/string.cpp
+++ string/string.cpp
@@ -2,95 +2,238 @@
 
 #include <cstdlib>
 #include <cstring>
+#include <iostream>
 #include <utility>
 
-String::String() {
-  str_ = static_cast<char*>(calloc(1, sizeof(char)));
-  capacity_ = 1;
-}
+String::String() { Reserve(1); }
 String::String(size_t size, char character) {
-  size_ = size;
-  capacity_ = size << 1;
-  str_ = static_cast<char*>(calloc(capacity_, sizeof(char)));
+  Resize(size);
   for (size_t i = 0; i < size; ++i) {
     str_[i] = character;
   }
 }
-String::String(char* str) {
-  size_ = strlen(str);
-  capacity_ = size_ << 1;
-  str_ = static_cast<char*>(calloc(capacity_, sizeof(char)));
-  memcpy(str_, str, sizeof(char) * size_);
+String::String(char const* str) {
+  if (strlen(str) != 0) {
+    Resize(strlen(str));
+    memcpy(str_, str, sizeof(char) * size_);
+  } else {
+    Reserve(1);
+  }
 }
-String::String(String& other) {
+String::String(String const& other) {
   if (size_ < other.size_) {
     Resize(other.size_);
   }
   memcpy(str_, other.str_, other.size_);
 }
-String::~String() {
-  free(str_);
-}
-String& String::operator=(String& other) {
-  if (size_ < other.size_) {
-    Resize(other.size_);
-  }
+String& String::operator=(String const& other) {
+  Resize(other.size_);
   memcpy(str_, other.str_, other.size_);
   return *this;
 }
+String::~String() { free(str_); }
 
-void String::Clear() { size_ = 0; }
-
-void String::PushBack(char character) {
-  if (size_ == capacity_) {
-    Reserve(size_ << 1);
-  }
-  str_[size_] = character;
-  size_ += 1;
-}
-void String::PopBack() { size_ -= 1; }
-void String::Resize(size_t size) {
-  Reserve(size);
-  size_ = size;
-}
-void String::Reserve(size_t capacity) {
-  capacity_ = capacity;
-  char* old_str = str_;
-  str_ = static_cast<char*>(calloc(capacity_, sizeof(char)));
-  memcpy(str_, old_str, sizeof(char) * size_);
-  free(old_str);
-}
-void String::ShrinkToFit() {
-  char* old_str = str_;
-  str_ = static_cast<char*>(calloc(size_, sizeof(char)));
-  memcpy(str_, old_str, sizeof(char) * size_);
-  free(old_str);
-  capacity_ = size_;
-}
-
-void String::Swap(String& other) {
-  std::swap(str_, other.str_);
-  std::swap(size_, other.size_);
-  std::swap(capacity_, other.capacity_);
-}
-
-
-String String::operator+(String other) {
+String String::operator+(String const& other) const {
   String buf = *this;
   buf += other;
   return buf;
 }
-String String::operator+=(String other) {
+String& String::operator+=(String const& other) {
   if (other.size_ + size_ > capacity_) {
-    Reserve((other.size_ + size_) << 1);
+    Reserve(other.size_ + size_);
+    memcpy(str_ + size_ * sizeof(char), other.str_, other.size_ * sizeof(char));
+    Resize(size_ + other.size_);
+  } else {
+    memcpy(str_ + size_ * sizeof(char), other.str_, other.size_ * sizeof(char));
   }
-  memcpy(str_ + other.size_, other.str_, other.size_ * sizeof(char));
   return *this;
 }
 
-String String::operator*(size_t mult) {
-
+String String::operator*(size_t mult) const {
+  String buf = *this;
+  buf *= mult;
+  return buf;
 }
-String String::operator*=(size_t mult) {
+String& String::operator*=(size_t mult) {
+  if (mult == 0) {
+    Clear();
+  } else {
+    Reserve(mult * size_);
+    for (size_t i = 1; i < mult; ++i) {
+      memcpy(str_ + i * size_ * sizeof(char), str_, size_ * sizeof(char));
+    }
+    Resize(mult * size_);
+  }
+  return *this;
+}
 
+bool String::operator==(String const& other) const {
+  if (size_ != other.size_) {
+    return false;
+  }
+
+  for (size_t i = 0; i < size_; ++i) {
+    if (str_[i] != other.str_[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+bool String::operator!=(String const& other) const {
+  return not(other == *this);
+}
+
+bool String::operator<(String const& other) const {
+  for (size_t i = 0; i < std::min(size_, other.size_); ++i) {
+    if (str_[i] < other.str_[i]) {
+      return true;
+    }
+  }
+  return size_ < other.size_;
+}
+bool String::operator>(String const& other) const {
+  for (size_t i = 0; i < std::min(size_, other.size_); ++i) {
+    if (str_[i] > other.str_[i]) {
+      return true;
+    }
+  }
+  return size_ > other.size_;
+}
+
+bool String::operator<=(String const& other) const {
+  for (size_t i = 0; i < std::min(size_, other.size_); ++i) {
+    if (str_[i] < other.str_[i]) {
+      return true;
+    }
+    if (str_[i] > other.str_[i]) {
+      return false;
+    }
+  }
+  return size_ <= other.size_;
+}
+bool String::operator>=(String const& other) const {
+  for (size_t i = 0; i < std::min(size_, other.size_); ++i) {
+    if (str_[i] > other.str_[i]) {
+      return true;
+    }
+    if (str_[i] < other.str_[i]) {
+      return false;
+    }
+  }
+  return size_ >= other.size_;
+}
+
+std::vector<String> String::Split(String const& delim) const {
+  std::vector<String> res;
+  size_t to_compare = 0;
+  String buff;
+
+  for (size_t i = 0; i < size_; i++) {
+    if (str_[i] == delim[to_compare]) {
+      to_compare += 1;
+    } else {
+      to_compare = 0;
+    }
+    buff.PushBack(str_[i]);
+    if (to_compare == delim.Size()) {
+      buff.Resize(buff.Size() - delim.Size());
+      res.push_back(buff);
+      buff.Clear();
+      to_compare = 0;
+    }
+  }
+  res.push_back(buff);
+  return res;
+}
+
+String String::Join(std::vector<String> const& strings) const {
+  if (strings.empty()) {
+    return "";
+  }
+
+  String res = strings[0];
+  for (size_t i = 1; i < strings.size(); ++i) {
+    res += *this;
+    res += strings[i];
+  }
+  return res;
+}
+
+std::istream& operator>>(std::istream& in, String& str) {
+  str.Clear();
+  char next = ' ';
+
+  while (next == ' ') {
+    in.get(next);
+  }
+
+  str.PushBack(next);
+  while (in.get(next)) {
+    if (next == ' ') {
+      break;
+    }
+    str.PushBack(next);
+  }
+  return in;
+}
+std::ostream& operator<<(std::ostream& out, String const& str) {
+  for (size_t i = 0; i < str.size_; ++i) {
+    out << str.str_[i];
+  }
+  return out;
+}
+
+void String::PushBack(char character) {
+  if (size_ == capacity_) {
+    Reserve(capacity_ << 1);
+  }
+  str_[size_] = character;
+  size_ += 1;
+}
+void String::PopBack() {
+  if (size_ == 0) {
+    return;
+  }
+  size_ -= 1;
+}
+
+void String::Clear() { size_ = 0; }
+void String::Resize(size_t size) {
+  Reserve(size);
+  size_ = size;
+  str_[size_] = '\0';
+}
+void String::Resize(size_t size, char character) {
+  Reserve(size);
+  for (size_t i = size_; i < size; ++i) {
+    str_[i] = character;
+  }
+  size_ = size;
+  str_[size_] = '\0';
+}
+void String::Reserve(size_t capacity) {
+  if (capacity_ >= capacity) {
+    return;
+  }
+  capacity_ = capacity;
+  char* old_str = str_;
+  str_ = static_cast<char*>(calloc(capacity_ + 1, sizeof(char)));
+  memcpy(str_, old_str, sizeof(char) * size_);
+  free(old_str);
+}
+void String::ShrinkToFit() {
+  if (size_ == capacity_) {
+    return;
+  }
+  char* old_str = str_;
+  str_ = static_cast<char*>(calloc(size_ + 1, sizeof(char)));
+  memcpy(str_, old_str, sizeof(char) * size_);
+  free(old_str);
+  capacity_ = size_;
+  str_[size_] = '\0';
+}
+void String::Swap(String& other) {
+  std::swap(str_, other.str_);
+  std::swap(size_, other.size_);
+  std::swap(capacity_, other.capacity_);
 }
--- string/string.hpp
+++ string/string.hpp
@@ -1,44 +1,63 @@
 #include <cstdlib>
+#include <iostream>
+#include <vector>
 
 class String {
-  public:
-    String();
-    String(size_t size, char character);
-    String(char*);
+ public:
+  String();
+  String(size_t size, char character);
+  String(char const*);
+  String(String const&);
+  String& operator=(String const&);
+  ~String();
 
-    String(String&);
-    ~String();
-    String& operator=(String&);
+  char& operator[](size_t idx) { return *(str_ + idx); }
+  char operator[](size_t idx) const { return str_[idx]; }
+  char& Front() { return str_[0]; }
+  char Front() const { return str_[0]; }
+  char& Back() { return str_[size_ - 1]; }
+  char Back() const { return str_[size_ - 1]; }
 
+  bool Empty() const { return size_ == 0; }
+  size_t Size() const { return size_; }
+  size_t Capacity() const { return capacity_; }
+  char* Data() { return str_; }
+  char const* Data() const { return str_; }
 
-    void Clear();
-    void PushBack(char);
-    void PopBack();
-    void Resize(size_t);
-    void Reserve(size_t);
-    void ShrinkToFit();
-    void Swap(String&);
+  String operator+(String const&) const;
+  String& operator+=(String const&);
 
-    char& operator[](size_t idx) { return str_[idx]; }
-    char operator[](size_t idx) const { return str_[idx]; }
-    char& Front() { return str_[0]; }
-    char& Back() { return str_[size_ - 1]; }
-    char Front() const { return str_[0]; }
-    char Back() const { return str_[size_ - 1]; }
+  String operator*(size_t) const;
+  String& operator*=(size_t);
 
-    bool Empty() const { return size_ == 0; }
-    size_t Size() const { return size_; }
-    size_t Capacity() const { return capacity_; }
-    char* Data() const { return str_; }
+  bool operator<(String const&) const;
+  bool operator>(String const&) const;
+  bool operator<=(String const&) const;
+  bool operator>=(String const&) const;
+  bool operator==(String const&) const;
+  bool operator!=(String const&) const;
 
-    String operator+(String);
-    String operator+=(String);
+  std::vector<String> Split(String const& = " ") const;
+  String Join(std::vector<String> const& strings) const;
 
-    String operator*(size_t);
-    String operator*=(size_t);
+  friend std::istream& operator>>(std::istream&, String&);
+  friend std::ostream& operator<<(std::ostream&, String const&);
 
-  private:
-    char* str_ = nullptr;
-    size_t size_ = 0;
-    size_t capacity_ = 0;
+  void PushBack(char);
+  void PopBack();
+
+  void Clear();
+  void Resize(size_t);
+  void Resize(size_t, char);
+  void Reserve(size_t);
+  void ShrinkToFit();
+  void Swap(String&);
+
+ private:
+  char* str_ = nullptr;
+  size_t size_ = 0;
+  size_t capacity_ = 0;
 };
+
+std::istream& operator>>(std::istream&, String&);
+std::ostream& operator<<(std::ostream&, String const&);
