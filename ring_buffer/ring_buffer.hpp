191ee91f9afa19140c2603ef4fc1ac9666b5f0f4
Иван Чай <138519172+iv4n-t3a@users.noreply.github.com>
2023-11-12 08:34:18 +0000

    Fix codestyle indentations

--- ring_buffer/ring_buffer.hpp
+++ ring_buffer/ring_buffer.hpp
@@ -2,38 +2,38 @@
 #include <vector>
 
 class RingBuffer {
-  public:
-    explicit RingBuffer(size_t capacity) : buffer_(capacity) {}
+ public:
+  explicit RingBuffer(size_t capacity) : buffer_(capacity) {}
 
-    size_t Size() const { return size_; }
-    bool Empty() const { return Size() == 0; }
+  size_t Size() const { return size_; }
+  bool Empty() const { return Size() == 0; }
 
-    bool TryPush(int element) {
-      if (IsOverflow()) {
-        return false;
-      }
-      buffer_[end_] = element;
-      end_ = NextIndex(end_);
-      size_ += 1;
-      return true;
+  bool TryPush(int element) {
+    if (IsOverflow()) {
+      return false;
     }
+    buffer_[end_] = element;
+    end_ = NextIndex(end_);
+    size_ += 1;
+    return true;
+  }
 
-    bool TryPop(int* element) {
-      if (Size() == 0) {
-        return false;
-      }
-      *element = buffer_[begin_];
-      begin_ = NextIndex(begin_);
-      size_ -= 1;
-      return true;
+  bool TryPop(int* element) {
+    if (Size() == 0) {
+      return false;
     }
+    *element = buffer_[begin_];
+    begin_ = NextIndex(begin_);
+    size_ -= 1;
+    return true;
+  }
 
-  private:
-    bool IsOverflow() const { return size_ == buffer_.size(); }
-    size_t NextIndex(size_t index) { return (index + 1) % buffer_.size(); }
+ private:
+  bool IsOverflow() const { return size_ == buffer_.size(); }
+  size_t NextIndex(size_t index) { return (index + 1) % buffer_.size(); }
 
-    size_t begin_ = 0;
-    size_t end_ = 0;
-    size_t size_ = 0;
-    std::vector<int> buffer_;
+  size_t begin_ = 0;
+  size_t end_ = 0;
+  size_t size_ = 0;
+  std::vector<int> buffer_;
 };
